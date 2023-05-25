cc5ca9cbabd396e15f6f9ebdd953ca422bb00446
pinof <fattoruso.g@gmail.com>
2023-05-25 21:43:38 +0200

    relazioni

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Contact;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
@@ -12,7 +13,29 @@ class ContactController extends Controller
      */
     public function index()
     {
-        //
+        /*  Esempio 1  */
+        $contactIds = [1,2];
+        $contactToAssociate = Contact::whereIn('id', $contactIds)->get();
+        collect($contactToAssociate)->each(function ($contact){
+            $contact->user()->associate(User::find(1))->save();
+            #$contact->user()->dissociate(User::find(1))->save();
+        });
+
+
+        /* Esempio 2 */
+        $userContacts = User::find(1)->contacts()->where('qualification','impiegato')->get();
+
+
+        /* Esempio 3 */
+        $userWithRelation = User::find(1)->has('contacts')->get(); #controlla se c'è una relazione Classe User e Contacts
+
+
+        #Esempio 4
+        $userWithRelationAndQueryCustom = Contact::whereHas('phoneNumber', function($query){
+            $query->where('phone', 'like', '%3%');
+        })->get();
+
+        return $userWithRelationAndQueryCustom;
     }
 
     /**
--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Contact extends Model
@@ -13,5 +15,10 @@ class Contact extends Model
     public function phoneNumber(): HasOne
     {
            return $this->hasOne(PhoneNumber::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
